937c3f07422d8832e5192fafb770fa14b22537a9
Lepotototor <victor.flament@protonmail.com>
2023-04-10 21:33:23 +0200

    first commit

---
+++ gpt.py
@@ -0,0 +1,43 @@
+import openai
+
+class GPT:
+    def __init__(self, key):
+        # Définir votre clé API
+        openai.api_key = key
+
+    def reponse_a_question(self, question):
+        # Définir le modèle à utiliser
+        model_engine = "text-davinci-002"
+        #model_engine = "davinci"
+
+        # Définir la phrase de départ pour générer du texte
+        prompt = question
+
+        completions = openai.Completion.create(
+            engine=model_engine,
+            prompt=prompt,
+            max_tokens=250,
+            n=1,
+            stop=None,
+            temperature=1,
+            presence_penalty = 1
+        )
+
+        print(completions)
+        return completions.choices[0].text
+
+        # Afficher la réponse
+        print(response.choices[0].text)
+
+
+
+
+test = GPT("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")
+
+"""
+question = "Qui est tu ?"
+
+while question != "exit":
+    print(test.reponse_a_question(question))
+    question = input("Quelle est votre question ? ")
+"""
